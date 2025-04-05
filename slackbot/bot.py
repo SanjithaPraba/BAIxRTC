4302bd986da5f54b29b877fa282108ad3ac455b6
Shriya Dale <shriyadale85@gmail.com>
2025-04-05 16:41:13 -0400

    hardcoded file paths

--- slackbot/bot.py
+++ slackbot/bot.py
@@ -4,6 +4,8 @@ from pathlib import Path
 from dotenv import load_dotenv
 from flask import Flask
 from slackeventsapi import SlackEventAdapter
+import sys
+sys.path.append("c:/Users/shriy/OneDrive/Desktop/Experiential/projects/BAIxRTC") 
 from LangGraph.common_workflow import QueryState, retrieve_context, generate_response, should_respond
 
 env_path = Path('.') / '.env'
@@ -15,22 +17,12 @@ slack_events_adapter = SlackEventAdapter(os.environ["SLACK_SIGNING_SECRET"], "/s
 client = slack.WebClient(token=os.environ["SLACK_TOKEN"])
 BOT_ID = client.api_call("auth.test")["user_id"]
 
-#read + respond to msges
 @slack_events_adapter.on('message')
 def message(payload):
     event = payload.get('event', {})
     channel_id = event.get('channel')
-    user_id = event.get('user')
-    text = event.get('text')
-    thread_ts = event.get('ts') or event.get('thread_ts')
 
-    state = QueryState(question=text)
-    state = should_respond(state)
-
-    if state.category == "should respond" and BOT_ID != user_id:
-        state = retrieve_context(state)
-        state = generate_response(state)
-        client.chat_postMessage(channel=channel_id, text=state.response, thread_ts=thread_ts)
+    client.chat_postMessage(channel=channel_id, text="Hello! I'm here to help you.")
 
 @app.route('/help', methods=['POST']) #command for intro for the slack bot
 def help():
